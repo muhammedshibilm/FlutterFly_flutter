a19f83020393d69b78e3bdaf6496ca7c642bd466
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-07-11 21:31:44 +0530

    Task seven is completed

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_project/db/service/authentication_service.dart';
 import 'package:flutter_project/db/model/user.dart';
