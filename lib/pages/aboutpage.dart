7dd908bebd5603cc83b4d6f7bfa2e0787134d0c7
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-07-13 15:29:25 +0530

    Task eight is completed

--- lib/pages/aboutpage.dart
+++ lib/pages/aboutpage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Aboutpage extends StatelessWidget {
-  const Aboutpage({super.key});
+  Aboutpage({super.key});
 
+  final _phone = "+919876543210";
+  final _url = Uri.parse("https://flutter.dev");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,10 +15,64 @@ class Aboutpage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "About page",
-          style: TextStyle(fontSize: 30),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            InkWell(
+              onTap: () async {
+                final url = Uri(scheme: "tel", path: _phone);
+                if (await canLaunchUrl(url)) {
+                  launchUrl(url);
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Contact me ",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Icon(
+                        Icons.phone_forwarded,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                launchUrl(_url, mode: LaunchMode.inAppBrowserView);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Visite ",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Icon(
+                        Icons.arrow_right_alt_rounded,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
